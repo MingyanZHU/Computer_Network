07d242e0873f3e805a8e9df7a5163d77da8e6547
1160300314 <hiterzmy@outlook.com>
2018-09-26 19:26:22 +0800

    增加2018年计算机网络实验指导书

--- Week_2_Application_Layer_DAYTIME_service/TCPDayClient.py
+++ Week_2_Application_Layer_DAYTIME_service/TCPDayClient.py
@@ -1,7 +1,10 @@
 import socket
 serverName = '120.79.190.92'
-serverPort = 13 # Get DAYTIME service
+# 阿里云服务器的ip地址
+serverPort = 13 
+# Get DAYTIME service
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#建立TCP连接
 clientSocket.connect((serverName, serverPort))
 
 while(True):
