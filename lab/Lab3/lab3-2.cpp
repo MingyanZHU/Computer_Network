386370ea14fe5084c99e6748da89608171e7a417
1160300314 <hiterzmy@outlook.com>
2018-11-08 09:30:12 +0800

    完成lab3-2

--- lab/Lab3/lab3-2.cpp
+++ lab/Lab3/lab3-2.cpp
@@ -5,7 +5,7 @@
 #include "sysInclude.h"
 #include <stdio.h>
 #include <vector>
-
+using std::vector;
 // system support
 extern void fwd_LocalRcv(char *pBuffer, int length);
 
@@ -16,12 +16,6 @@ extern void fwd_DiscardPkt(char *pBuffer, int type);
 extern unsigned int getIpv4Address();
 
 // implemented by students
-typedef struct stud_route_msg
-{
-	unsigned int dest;
-	unsigned int masklen;
-	unsigned int nexthop;
-} stud_route_msg;
 
 vector<stud_route_msg> route;
 
@@ -33,7 +27,15 @@ void stud_Route_Init()
 
 void stud_route_add(stud_route_msg *proute)
 {
-	route.insert(route.end(), proute);
+
+	stud_route_msg temp;
+	unsigned int dest = ntohl(proute->dest);
+	unsigned int masklen = ntohl(proute->masklen); // 24 extra
+	unsigned int nexthop = ntohl(proute->nexthop);
+	temp.dest = dest;
+	temp.masklen = masklen;
+	temp.nexthop = nexthop;
+	route.push_back(temp);
 	return;
 }
 
@@ -45,7 +47,7 @@ int stud_fwd_deal(char *pBuffer, int length)
 	short checksum = ntohs(*(unsigned short *)(pBuffer + 10)); // 10 byte all 8 bits
 	int destination = ntohl(*(unsigned int *)(pBuffer + 16));  // 16 byte 4 Bytes destination ip
 
-	ttl -= 1;
+	// ttl -= 1;
 
 	if (ttl <= 0)
 	{
@@ -61,18 +63,15 @@ int stud_fwd_deal(char *pBuffer, int length)
 	}
 
 	stud_route_msg *ans_route = NULL;
-	unsigned int ans = 0;
 	int temp_dest = destination;
 	for (int i = 0; i < route.size(); i++)
 	{
-		unsigned int temp_sub_net = route[i].dest & route[i].masklen;
-		while(!(temp_sub_net & 1)){
-			temp_dest >>= 1;
-			temp_sub_net >>= 1;
-		}
-		if (temp_sub_net == temp_dest){
-			ans = route[i].dest & route[i].masklen & destination;
+		unsigned int temp_sub_net = route[i].dest & ((1 << 31) >> (route[i].masklen - 1));
+
+		if (temp_sub_net == temp_dest)
+		{
 			ans_route = &route[i];
+			break;
 		}
 	}
 
@@ -83,15 +82,17 @@ int stud_fwd_deal(char *pBuffer, int length)
 	}
 	else
 	{
-		memset(pBuffer + 10, 0, 2);
-		pBuffer[8] = ttl;
+		char *buffer = new char[length];
+		memcpy(buffer, pBuffer, length);
+		buffer[8] = ttl - 1;
+		memset(buffer + 10, 0, 2);
 		unsigned long sum = 0;
 		unsigned long temp = 0;
 
-		for (int i = 0; i < head_length; i += 2)
+		for (int i = 0; i < head_length * 2; i++)
 		{
-			temp += (unsigned char)pBuffer[i] << 8;
-			temp += (unsigned char)pBuffer[i + 1];
+			temp += (unsigned char)buffer[i * 2] << 8;
+			temp += (unsigned char)buffer[i * 2 + 1];
 			sum += temp;
 			temp = 0;
 		}
@@ -100,8 +101,8 @@ int stud_fwd_deal(char *pBuffer, int length)
 		unsigned short checksum = l_word + h_word;
 		checksum = ~checksum;
 		unsigned short header_checksum = htons(checksum);
-		memcpy(pBuffer + 10, &header_checksum, 2);
-		fwd_SendtoLower(pBuffer, length, ans_route->nexthop);
+		memcpy(buffer + 10, &header_checksum, 2);
+		fwd_SendtoLower(buffer, length, ans_route->nexthop);
 	}
 	return 0;
 }
